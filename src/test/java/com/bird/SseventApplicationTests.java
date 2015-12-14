a1d016f3153ec44c78d67d916cff84b5b2f33656
Qunfei Wu <qunfei.wu@booxware.de>
2015-12-14 17:49:25 +0100

    Add client to test

--- src/test/java/com/bird/SseventApplicationTests.java
+++ src/test/java/com/bird/SseventApplicationTests.java
@@ -1,18 +1,43 @@
 package com.bird;
 
+import org.glassfish.jersey.media.sse.EventInput;
+import org.glassfish.jersey.media.sse.InboundEvent;
+import org.glassfish.jersey.media.sse.SseFeature;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SseventApplication.class)
 @WebAppConfiguration
 public class SseventApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void oneClient() {
+        Client client = ClientBuilder.newBuilder()
+                .register(SseFeature.class).build();
+        WebTarget target = client.target("http://localhost:8080/events");
+
+        EventInput eventInput = target.request().get(EventInput.class);
+        while (!eventInput.isClosed()) {
+            final InboundEvent inboundEvent = eventInput.read();
+            if (inboundEvent == null) {
+                // connection has been closed
+                break;
+            }
+            System.out.println(inboundEvent.getName() + "; "
+                    + inboundEvent.readData(String.class));
+        }
+    }
 
 }
